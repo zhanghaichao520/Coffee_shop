793a1587c6bbdd0e8b044e501d37e83fe3bf6969
haichao <zhang_haichao@163.com>
2022-12-02 16:21:37 +0800

    add dao del and update

--- src/main/java/edu/xjtlu/cpt403/util/RowData.java
+++ src/main/java/edu/xjtlu/cpt403/util/RowData.java
@@ -5,4 +5,24 @@ import java.util.*;
 
 public class RowData extends HashMap<String,String> implements Serializable {
 
+    private int rowIndex;
+
+    private Map<String, Integer> colNameToColIndex;
+
+
+    public Map<String, Integer> getColNameToColIndex() {
+        return colNameToColIndex;
+    }
+
+    public void setColNameToColIndex(Map<String, Integer> colNameToColIndex) {
+        this.colNameToColIndex = colNameToColIndex;
+    }
+
+    public int getRowIndex() {
+        return rowIndex;
+    }
+
+    public void setRowIndex(int rowIndex) {
+        this.rowIndex = rowIndex;
+    }
 }
