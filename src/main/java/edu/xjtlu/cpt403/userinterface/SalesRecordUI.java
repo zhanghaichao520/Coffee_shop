b9a302648b17165c6f02e6602cbc3c0dc8142fb4
Ivanwyw <976913034@qq.com>
2022-12-11 14:11:30 +0800

    Add SalesRecordUI

--- src/main/java/edu/xjtlu/cpt403/userinterface/SalesRecordUI.java
+++ src/main/java/edu/xjtlu/cpt403/userinterface/SalesRecordUI.java
@@ -15,6 +15,14 @@ public class SalesRecordUI {
 
 
     /**
+     * New sales record view function, default administrator privileges
+     * 1.Display of salesRecord statistics
+     * 2.The statistics table is divided into three types: <Day> <Week> <Month>
+     *   and the administrator can choose to view any one of them
+     * 3.Data displayed in the statistics table:
+     *   id, product id, product name, sales number,  sales amount, payTime
+     * 4.The statistics table for each dimension is preceded by the amount statistics and customer statistics
+     *
      * 新增销售记录查看功能，默认管理员权限
      *  1. 展示销售记录统计表
      *  2. 统计表分为<当日><本周><本月>三种类型，管理员可以选择查看其中任意一种
@@ -32,6 +40,7 @@ public class SalesRecordUI {
          }
 
          // n天前的时间戳
+         // TimeStamp before nDays
          long nDaysBeforeTimeStamp = System.currentTimeMillis() - nDays*24*60*60*1000L;
 
          List<SalesRecord> statisticResult = new ArrayList<>();
@@ -39,11 +48,13 @@ public class SalesRecordUI {
          for (SalesRecord salesRecord : salesRecordList) {
              long payTimeStamp = DateUtils.parseDate(salesRecord.getPayTime(), DateUtils.DATE_FORMAT_DAY).getTime();
              //取出销售记录表里面payTime=当前日期的数据
+             //Fetch the data with payTime=current date from the sales record table
              if (payTimeStamp >= nDaysBeforeTimeStamp) {
                  statisticResult.add(salesRecord);
              }
          }
          //统计指定时段内的收入和访客量
+         //Statistics on revenue and visitors during the specified period
          double totalIncome = 0D;
          Set<Integer> userIdSet = new HashSet<>();
          for(int i = 0; i < statisticResult.size(); i ++) {
